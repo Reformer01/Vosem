6cea39bd62fcbfc69173ee65a282b823fb5fa17a
Reformer Ejembi <reformer.ejembi@iworldnetworks.net>
2026-02-01 04:57:57 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1357235131-b01a2",
+  "appId": "1:915571989858:web:443319b5a7cc21598aa03b",
+  "apiKey": "AIzaSyAlfnF95Cx2PBXTFqtYiQzpEJ1XS2xx-hg",
+  "authDomain": "studio-1357235131-b01a2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "915571989858"
+};
