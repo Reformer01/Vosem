c2c7f736db8680be63207caac15abfc3e5a9a9fb
Reformer Ejembi <reformer.ejembi@iworldnetworks.net>
2026-02-04 08:33:27 +0000

    It is still redirecting to this page

--- next.config.ts
+++ next.config.ts
@@ -62,15 +62,6 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: '/verify-donation',
-        destination: '/payment-processing',
-        permanent: false,
-      },
-    ]
-  },
 };
 
 export default nextConfig;
